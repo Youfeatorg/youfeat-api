aa04abe5a827534d87cd5f7b76db8601773c9305
blaspace <js5618171@gmail.com>
2025-02-16 00:28:49 +0100

    testing

--- controler/handleVideoUploads.js
+++ controler/handleVideoUploads.js
@@ -1,42 +1,47 @@
-import fs from "fs"
-import User from "../schema/userSchema.js"
-import cloudinary from "cloudinary"
-import ffmpeg from "fluent-ffmpeg"
-import Video from "../schema/videoSchema.js"
-import bucket from "../Credentials/GoogleBucket.js"
+import fs from "fs";
+import User from "../schema/userSchema.js";
+import cloudinary from "cloudinary";
+import ffmpeg from "fluent-ffmpeg";
+import Video from "../schema/videoSchema.js";
+import bucket from "../Credentials/GoogleBucket.js";
 
 const uploadVideo = async (req, res) => {
-
   /*const result = await cloudinary.v2.uploader.upload(req.file.path, {
     resource_type: "video",
     folder: "/youfeat/",
     use_filename: true,
   });*/
 
-  /*const blob = bucket.file(req.file.path)
-  const blobStream = blob.createWriteStream()
-  blobStream.on("finish", (e)=>{
+  const blob = bucket.file(
+    req.file.originalname + Date.now() + "-" + Math.round(Math.random() * 1e9)
+  );
+  const blobStream = blob.createWriteStream();
+  blobStream.on("error", (e) => {
     console.log(e);
-  })
+    res.sendStatus(500);
+  });
 
-  blobStream.end(req.file.buffer)
-*/
-  const user = await User.findByIdAndUpdate(req.body.userId, {
-    video: {
-      filename: req.file.filename,
-      contentType: req.file.mimetype,
+  blobStream.on("finish", async (e) => {
+    const user = await User.findByIdAndUpdate(req.body.userId, {
+      video: {
+        filename: req.file.filename,
+        filepath: `https://storage.googleapis.com/youfea-tvideo_bucketcom/${blob.name}`,
+        contentType: req.file.mimetype,
+        title: req.body.title,
+        catigory: req.body.catigory,
+        description: req.body.description,
+      },
+    });
+    const i = await Video.create({
+      video:  `https://storage.googleapis.com/youfea-tvideo_bucketcom/${blob.name}`,
       title: req.body.title,
-      catigory: req.body.catigory,
       description: req.body.description,
-    },
+      published: false,
+    });
   });
-  const i = await Video.create({
-    video: req.file.filename,
-    title: req.body.title,
-    description: req.body.description,
-    published: false,
-  });
-  res.send(user)
+  blobStream.end(req.file.buffer);
+
+  res.sendStatus(200);
 };
 
 export default uploadVideo;
