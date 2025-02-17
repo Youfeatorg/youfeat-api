78f14a901d8dbf188f4c1f06eaede42f017c8591
blaspace <js5618171@gmail.com>
2025-02-17 22:06:17 +0100

    hhhh

--- controler/GetThumbnail.js
+++ controler/GetThumbnail.js
@@ -1,19 +1,38 @@
-import vimeo from "../Credentials/Vimeo.js"
-import User from "../schema/userSchema.js"
+import vimeo from "../Credentials/Vimeo.js";
+import User from "../schema/userSchema.js";
 
-const GetThumbnail =async (req, res)=> {
-    const { id } = req.params
-    const {path} = req.body
-
-    const uservideo = await User.findById(id)
-    vimeo.request({ method: "GET", path }, async (err, body) => {
-        const user = await User.findByIdAndUpdate(id, {
-            video: {
-                ...uservideo.video, thumbnail: body.pictures.sizes[body.pictures.sizes.length - 1].link_with_play_button
-            }
-        })
-        res.sendStatus(200)
-    })
+const del = async() => {
+  /*vimeo.request(
+    { method: "GET", path: "/videos/1057634991" },
+    async (err, body) => {
+      console.log(
+        body.pictures.sizes[body.pictures.sizes.length - 1]
+          .link_with_play_button
+      );
+    }
+  );*/
 }
+  
+const GetThumbnail = async (req, res) => {
+  const { id } = req.params;
+  const { path } = req.body;
 
-export default GetThumbnail
+  const uservideo = await User.findById(id);
+  vimeo.request({ method: "GET", path }, async (err, body) => {
+    if (!err) {
+        setTimeout(async()=>{
+    const user = await User.findByIdAndUpdate(id, {
+      video: {
+        ...uservideo.video,
+        thumbnail:
+          body.pictures.sizes[body.pictures.sizes.length - 1]
+            .link_with_play_button,
+      },
+    });
+    res.sendStatus(200);
+}, 10000)
+    }
+  });
+};
+
+export default GetThumbnail;
