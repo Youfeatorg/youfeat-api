bd28ffdcc7ba743002a1fa8812a5c6821101635a
blaspace <js5618171@gmail.com>
2025-02-17 23:15:52 +0100

    hhhh

--- controler/GetThumbnail.js
+++ controler/GetThumbnail.js
@@ -12,27 +12,38 @@ const del = async() => {
     }
   );*/
 }
+let i = "/videos/1057634991"
+
   
 const GetThumbnail = async (req, res) => {
-  const { id } = req.params;
-  const { path } = req.body;
 
-  const uservideo = await User.findById(id);
-  vimeo.request({ method: "GET", path }, async (err, body) => {
-    if (!err) {
-        setTimeout(async()=>{
-    const user = await User.findByIdAndUpdate(id, {
-      video: {
-        ...uservideo.video,
-        thumbnail:
-          body.pictures.sizes[body.pictures.sizes.length - 1]
-            .link_with_play_button,
-      },
-    });
-    res.sendStatus(200);
-}, 10000)
-    }
-  });
+    const user = await User.findById(req.params.id)
+        const maxAttempts = 10;
+        const delay = 10000; // 10 seconds
+      
+        for (let i = 0; i < maxAttempts; i++) {
+          const thumbnailUrl = getThumbnailUrl(req.body.path.split('/')[2], '1280x720');
+          const isThumbnailReady = await checkThumbnailExists(thumbnailUrl);
+      
+          if (isThumbnailReady) {
+            await User.findByIdAndUpdate(req.params.id, {video: {...user.video, thumbnail: thumbnailUrl }});
+            res.sendStatus(200)
+          }
+      
+          console.log('Thumbnail not ready yet. Retrying...');
+          await new Promise((resolve) => setTimeout(resolve, delay));
+        }
+      
+        throw new Error('Thumbnail not available after multiple attempts.');
+      
+      async function checkThumbnailExists(url) {
+        try {
+          const response = await fetch(url, { method: 'HEAD' });
+          return response.ok;
+        } catch (error) {
+          return false;
+        }
+      }
 };
 
 export default GetThumbnail;
