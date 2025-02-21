c7cd036926d3f048c3c98f8bf43b0caed079666b
blaspace <js5618171@gmail.com>
2025-02-21 11:23:19 +0100

    added video size limit

--- Credentials/Vimeo.js
+++ Credentials/Vimeo.js
@@ -1,10 +1,9 @@
 import Vimeo from "vimeo"
 
 // Replace these with your Vimeo API credentials
-const CLIENT_ID = "e9630114f474e51c1045f84b9b978b67514bcf35";
-const CLIENT_SECRET =
-  "5uF8SBrSdqI/LkZMP+sp6r2AqB5GApyorxvOqL9jR0k8XihHof+1iNGgdwPsdcdqeqV4EWs77ZcfuDp6SjtNLOy/quVHJK8/6qtmwHiI+D0YpS9kEDVmzuhlWf2vaMUz";
-const ACCESS_TOKEN = "ce08da22c18ddceeae427885826784bd";
+const CLIENT_ID = process.env.VIMEO_CLIENT_ID
+const CLIENT_SECRET = process.env.VIMEO_CLIENT_SECRET
+const ACCESS_TOKEN = process.env.VIMEO_ACCESS_TOKEN
 
 // Initialize the Vimeo client
 const vimeo = new Vimeo.Vimeo(CLIENT_ID, CLIENT_SECRET, ACCESS_TOKEN);
