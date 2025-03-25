ac4a4d7a0eae661a6d8b0d215fb2019faef9a208
blaspace <js5618171@gmail.com>
2025-03-25 15:21:27 +0100

    hh

--- controler/handleComment.js
+++ controler/handleComment.js
@@ -1,13 +1,14 @@
 import Comment from "../schema/comment.js"
 
 const handleComment = async(req, res) => {
-  const { comment, userId, commenterId } = req.body;
+  const { comment, userId, commenterId, videoId } = req.body;
 try{
   const newComment = await Comment.create({
     comment,
     userId,
     commenterId,
     under: "root",
+    videoId
   });
   res.send(newComment)
 }catch (err){
