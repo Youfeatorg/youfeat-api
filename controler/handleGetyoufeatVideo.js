d27f9e946df353ac5d419356cb335317f2c54974
blaspace <js5618171@gmail.com>
2025-05-19 14:21:10 +0100

    hello 222

--- controler/handleGetyoufeatVideo.js
+++ controler/handleGetyoufeatVideo.js
@@ -1,12 +1,12 @@
-import Video from "../schema/videoSchema.js"
+import Video from "../schema/videoSchema.js";
 
-const handleGetYoufeatVideo =async (req, res)=>{
-    try{
-    const videos = await Video.find()
-    res.send(videos)
-    }catch (err){
-        console.log(err);
-    }
-}
+const handleGetYoufeatVideo = async (req, res) => {
+  try {
+    const videos = await Video.find();
+    res.send(videos.sort(() => Math.random() - 0.5));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export default handleGetYoufeatVideo
+export default handleGetYoufeatVideo;
